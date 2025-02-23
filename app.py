083de377b2a562b4c2c6e160e67526dac11b0348
jagadeeswarudu <96833531+jagadeeswarudu@users.noreply.github.com>
2025-02-23 12:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template('index.html', review=review)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=5000,host="0.0.0.0")
